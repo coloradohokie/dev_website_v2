5aaadefe1d6ee00539ea30a06b3c518784e47b9e
Michael Newman <coloradohokie@gmail.com>
2020-10-23 12:38:12 -0600

    add skills section; add experience section

--- src/containers/Resume/Resume.js
+++ src/containers/Resume/Resume.js
@@ -20,17 +20,20 @@ const resume = (props) => {
                 </div>
                 <div className={classes.element2}>
                     <h2><i className="fas fa-tools"></i> Experience</h2>
+                    <Experience company="Skilled Inc" title="Instructional Coach" dates="2020">
+                        Coached students on data structures in JavaScript.
+                    </Experience>                    
                     <Experience company="Statline" title="Business Analyst" dates="2016 - 2019">
-                        Did Business Analyst stuff.
+                        Authored and managed software business requirements for front end and back end; conducted usability studies; modernized the user experience
                     </Experience>
                     <Experience company="Charles Schwab" title="Business Analyst" dates="2014 - 2015">
-                        Did business analyst stuff
+                        Managed software compliance documentation and customer communication for a new product
                     </Experience>
                     <Experience company="McKesson" title="Senior Business Analyst" dates="2008 - 2014">
-                        Did business analyst stuff
+                        Authored and managed user stories for a variety of of applications in McKesson's enterprise software - Horizon Clincals.
                     </Experience>
-                    <Experience company="Optiframe, LLC" title="Product Manager" dates="2003 - 2008">
-                        Did business analyst stuff
+                    <Experience company="OptiFrame, LLC" title="Product Manager" dates="2003 - 2008">
+                        Oversaw human factors design and product usability in OptiFrame's residential building construction software.
                     </Experience>
                 </div>
                 <div className={classes.element3}>
@@ -57,17 +60,52 @@ const resume = (props) => {
                     <div className="circle"></div>
                     <h1>My Skills</h1>
                 </div>
+
                 <div className={classes.element2}>
-                    <h2><i className="fas fa-drafting-compass"></i> Design</h2>
+                    <h2><i className="fas fa-drafting-compass"></i> Tools </h2>
+                    <ul>
+                        <li>GitHub</li>
+                        <li>Firebase</li>
+                        <li>Heroku</li>
+                        <li>Adobe XD</li>
+                        <li>Balsamiq</li>
+                        <li>Rally</li>
+                        <li>JIRA</li>
+                        <li>Trello</li>
+                        <li>Team Foundation Server (TFS)</li>
+                        <li>VS Code</li>
+                    </ul>
                 </div>
                 <div className={classes.element3}>
-                    <h2><i className="fas fa-language"></i> Languages</h2>
-                </div>
-                <div className={classes.element2}>
                     <h2><i className="fas fa-laptop-code"></i> Coding</h2>
+                    <ul>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Ruby / Rails</li>
+                        <li>SQL / Postgres</li>
+                        <li>HTML</li>
+                        <li>CSS/SASS</li>
+                        <li>ActiveRecord</li>
+                        <li>Node.js</li>
+                        <li>Java</li>
+                    </ul>
                 </div>
-                <div className={classes.element3}>
+                <div className={classes.element4}>
                     <h2><i className="far fa-lightbulb"></i> Knowledge</h2>
+                    <div className={classes.knowledgeList}>
+                        <ul>
+                            <li>Agile Development</li>
+                            <li>Project Management</li>
+                            <li>Prototyping</li>
+                            <li>Presentations</li>
+                        </ul>
+                        <ul>
+                            <li>Requirements Elicitation</li>   
+                            <li>User Centered Design</li>
+                            <li>User Stories</li>
+                            <li>Waterfall Development</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
